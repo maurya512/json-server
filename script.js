558336a5aa63715106ee20b4588bb61abd7a25a9
maurya512 <patelmaurya0512@gmail.com>
2022-01-31 16:37:56 -0500

    shows all the todos in the list

--- script.js
+++ script.js
@@ -3,12 +3,77 @@
 let newTask = document.getElementById('newTask');
 let startDate = document.getElementById('startDate');
 let endDate = document.getElementById('endDate');
+let addTask = document.getElementById('addTask');
+let myTable = document.getElementById('myTasks');
 
 // ! storing the basic url in a variable to allow easy access
-const url = "http://localhost:3000/events"
+// const url = "http://localhost:3000/events"
 
 // * method to get all the tasks and displaying them on the screen
-fetch(url)
-.then((res) => res.json() )
-.then((json) => console.log(json))
+function fetchTasks() {
+    fetch("http://localhost:3000/events")
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data)
+            data.forEach((user) => {
+                newTask.innerText += user.eventName;
+                console.log(newTask);
+                startDate.innerText += user.startDate;
+                console.log(startDate);
+                endDate.innerText += user.endDate;
+            })
+        })
+}
 
+// ! invoking the loading of all tasks whenever the page is loaded
+window.addEventListener('load', fetchTasks);
+
+    // // <!-- <script>
+    //   // Get all Events
+    //   fetch("http://localhost:3000/events")
+    //     .then((response) => response.json())
+    //     .then((json) => console.log(json));
+
+    //   // Create a new Event
+    //   fetch("http://localhost:3000/events", {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //       Accept: "application/json",
+    //     },
+    //     body: JSON.stringify({
+    //       eventName: "TEST",
+    //       startDate: "1641790800000",
+    //       endDate: "1641790800000",
+    //     }),
+    //   })
+    //     .then((response) => response.json())
+    //     .then((json) => console.log(json));
+
+    //   // Delete an exist Event
+    //   fetch("http://localhost:3000/events/1", {
+    //     method: "DELETE",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //       Accept: "application/json",
+    //     },
+    //   })
+    //     .then((response) => response.json())
+    //     .then((json) => console.log(json));
+
+    //   //   // Update an exist Event
+    //   fetch("http://localhost:3000/events/2", {
+    //     method: "PUT",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //       Accept: "application/json",
+    //     },
+    //     body: JSON.stringify({
+    //       eventName: "TEST-CHANGED",
+    //       startDate: "1641790800000",
+    //       endDate: "1641790800000",
+    //     }),
+    //   })
+    //     .then((response) => response.json())
+    //     .then((json) => console.log(json));
+    // </script> -->
